a396a71c3400f3ba4a2d7617c763c8e807c0a55b
bjanani <vjanani@gmail.com>
2014-01-19 16:44:48 +0530

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,8 @@
+//to connect sql in python//
+ 
+import mysql.connector
+cn=mysql.connector connect(user='root',database='student',password='janujvk')
+cr=cn.execute("select.from mark")for(r,n,m,rs) in ce:
+ print("{0<5}|{1:<20}|{2:<5}|{3:>10}}|format{r,n,m,rs}
+cr.close();
+cn.close();
