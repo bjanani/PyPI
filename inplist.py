1f4f836b341a929dd00be3bf9a5da36b79284d8b
bjanani <vjanani@gmail.com>
2014-01-19 16:37:21 +0530

    Update and rename README.md to inplist.py

--- inplist.py
+++ inplist.py
@@ -1,8 +1,8 @@
 def inputlist():
  a=[[0,0,0],[0,0,0],[0,0,0]]
  for i in range(3):
-  for j in range(3);
-   a[i][j]=int(inpt("enter value"))
+  for j in range(3):
+   a[i][j]=int(input("enter value"))
  return a   
  
  a=inputlist()
